ff70b50e028c77dc90977b5cf61f31c7d42d6ece
AlexM <wmatallana28@gmail.com>
2025-09-03 19:00:54 -0500

    Repaso Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,76 @@
+fun main() {
+
+    // COMENTARIOS UNA LINEA
+
+    /*
+
+    comentarios multilinea
+
+     */
+
+
+    // VARIABLES -> var - variables que cambian
+    // CONSTANTES -> val - datos que no cambian
+
+
+    println("HOLA MUNDO")
+
+    // VARIBLES DE TIPO IMPLICITO
+/*
+    var numero = 56  // VARIABLE DE TIPO ENTERO
+
+    var decimal = 12.5   // VARIABLE DE TIPO DECIMAL
+
+    var texto = "REPASO KOTLIN"   //
+
+    val pi = 3.14    // CONSTANTE
+
+
+    println("VARIABLE DE TIPO ENTERO ${numero}")
+    println("VARIABLE DE TIPO DECIMAL ${decimal}")
+    println("VARIABLE DE TIPO TEXTO ${texto}")
+    println("CONSTANTE ${pi}")
+    */
+
+    //VARIABLES DE TIPO EXPLICITO
+
+    println("VARIABLES DE TIPO EXPLICITO")
+
+    var numero1:Int = 56
+
+    var decimal:Double = 12.5
+
+    println("VARIABLE DE TIPO ENTERO ${numero1}")
+    println("VARIABLE DE TIPO ENTERO ${decimal}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
